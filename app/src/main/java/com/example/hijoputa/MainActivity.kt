e98950f2e3f33114c10bed38d4f416c9b46be42e
xavierenti <xavier.massaneda@enti.cat>
2023-10-09 18:13:42 +0200

    calculadora

--- app/src/main/java/com/example/hijoputa/MainActivity.kt
+++ app/src/main/java/com/example/hijoputa/MainActivity.kt
@@ -1,137 +1,256 @@
-package com.example.hijoputa
+package com.example.apuntesclase
 
+import android.content.Context
+import android.graphics.Path.Op
 import android.os.Bundle
-import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.hijoputa.ui.theme.HijoputaTheme
+import com.example.hijoputa.R
 
 class MainActivity : ComponentActivity() {
 
-    enum class  Colors(val text: String, val number:Int, val color: Color){
-        Red("Rojo", 0, Color(255,0,0)),
-        Green("Green", 1, Color(0,255,0)),
-        Blue("Blue", 2, Color(0,0,255));
+    enum class Operations(val operation: (Int, Int) -> Int) {
+        Add({a, b -> a + b}),
+        Subtract({a, b, -> a - b}),
+        Multiply({a, b, -> a * b}),
+        Divide({a, b, -> a / b});
 
-        fun WithAlpha(alpha:Float): Color{
-            return this.color.copy(alpha)
+        public fun Operate(a: Int, b: Int) : Int {
+            return this.operation(a,b)
         }
     }
 
-    enum class Operations()
+    class ButtonsRow(var linearLayout : LinearLayout, val contenxt: Context) {
+        var buttons : MutableList<Button> = mutableListOf()
+
+        fun AddButtonToRow(text : String) : Button {
+            var bt = Button(contenxt)
+            bt.text = text
+            bt.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+            linearLayout.addView(bt)
+            buttons.add(bt)
+
+            return bt
+        }
+    }
+
+    class ButtonsGrid(var parent: LinearLayout, val contenxt : Context) {
+        // Necesita su Layout y el número de líneas que va a tener la GRID
+        val baseLinearLayout : LinearLayout
+        private var rows : MutableList<ButtonsRow> = mutableListOf()
+
+        init {
+            // Generamos el LinearLayout y lo asignamos a la vista padre
+            baseLinearLayout = LinearLayout(contenxt)
+            baseLinearLayout.orientation = LinearLayout.VERTICAL
+            parent.addView(baseLinearLayout)
+        }
+
+        fun AddNewRow() {
+            val linearLayout = LinearLayout(contenxt)
+            linearLayout.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+            baseLinearLayout.addView(linearLayout)
+            rows.add(ButtonsRow(linearLayout, contenxt))
+        }
+
+        fun GetRow(index : Int) : ButtonsRow? {
+            if (index >= rows.count() || index < 0) {
+                return null
+            }
+
+            return rows[index]
+        }
+    }
+
+    val result : TextView by lazy { findViewById(R.id.result) }
+    val buttonsContainer : LinearLayout by lazy { findViewById(R.id.buttons_container) }
+
+    var numB : Int = 0;
+    var numA : Int = 0;
+    var operation : Operations? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HijoputaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ){
-                    Column(){
-                        /*
-                        Greeting("Android")
-                        Greeting("Jodete")
-                        for(i in 1..10){Greeting("joputa")}
+        setContentView(R.layout.start_screen)
 
-                        Spacer(modifier = Modifier.height(16.dp))
+        val btGrid = ButtonsGrid(buttonsContainer, this)
 
-                        val names = listOf("conyo", "quantos", "nombres")
+        var names : MutableList<MutableList<String>> = mutableListOf()
+        names.add(mutableListOf("AC", "()", "%", "/"))
+        names.add(mutableListOf("7", "8", "9", "X"))
+        names.add(mutableListOf("4", "5", "6", "-"))
+        names.add(mutableListOf("1", "2", "3", "+"))
+        names.add(mutableListOf("0", ".", "<->", "="))
 
-                        for(name in names){
-                            Greeting(name)
+        for (y in names.indices) {
+            btGrid.AddNewRow()
+            var row : ButtonsRow? = btGrid.GetRow(y)
+            for (x in names[y].indices) {
+                row?.AddButtonToRow(names[y][x])?.setOnClickListener {
+                    when (names[y][x]) {
+                        "1" -> {
+
+                            if (operation == null) {
+                                numA = numA * 10 + 1
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 1
+                                result.text = numB.toString()
+                            }
+                        }
+                        "2" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 2
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 2
+                                result.text = numB.toString()
+                            }
+                        }
+                        "3" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 3
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 3
+                                result.text = numB.toString()
+                            }
+                        }
+                        "4" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 4
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 4
+                                result.text = numB.toString()
+                            }
+                        }
+                        "5" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 5
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 5
+                                result.text = numB.toString()
+                            }
+                        }
+                        "6" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 6
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 6
+                                result.text = numB.toString()
+                            }
+                        }
+                        "7" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 7
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 7
+                                result.text = numB.toString()
+                            }
+                        }
+                        "8" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 8
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 8
+                                result.text = numB.toString()
+                            }
+                        }
+                        "9" -> {
+                            if (operation == null) {
+                                numA = numA * 10 + 9
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB * 10 + 9
+                                result.text = numB.toString()
+                            }
+                        }
+                        "+" -> {
+                            operation = Operations.Add
+
+                            if (numB != 0) {
+                                result.text = operation?.Operate(numA,numB).toString()
+                                numA = operation?.Operate(numA, numB)!!
+                                numB = 0
+                            }
+                        }
+                        "-" -> {
+                            operation = Operations.Subtract
+                            if (numB != 0) {
+                                result.text = operation?.Operate(numA,numB).toString()
+                                numA = operation?.Operate(numA, numB)!!
+                                numB = 0
+                            }
+                        }
+                        "X" -> {
+                            operation = Operations.Multiply
+                            if (numB != 0) {
+                                result.text = operation?.Operate(numA,numB).toString()
+                                numA = operation?.Operate(numA, numB)!!
+                                numB = 0
+                            }
+                        }
+                        "/" -> {
+                            operation = Operations.Divide
+                            if (numB != 0) {
+                                result.text = operation?.Operate(numA,numB).toString()
+                                numA = operation?.Operate(numA, numB)!!
+                                numB = 0
+                            }
+                        }
+                        "=" -> {
+                            result.text = operation?.Operate(numA,numB).toString()
+                            numA = 0
+                            numB = 0
+                            operation = null
+                        }
+                        "<->" -> {
+                            if (operation == null) {
+                                numA = numA / 10
+                                result.text = numA.toString()
+                            }
+                            else {
+                                numB = numB / 10
+                                result.text = numB.toString()
+                            }
+                        }
+                        "AC" -> {
+                            numA = 0
+                            numB = 0
+                            operation = null
+
+                            result.text = "0"
                         }
 
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        for(i in names.indices){
-                            Greeting(name= "indice: " + i.toString())
-                        }
-
-                        Spacer(modifier = Modifier.height(16.dp))
-                        
-                        names.forEach{name -> 
-                            Greeting(name = name)
-                        }
-
-                        Spacer(modifier = Modifier.height(16.dp))
-                        
-                        names.forEachIndexed { index, name ->  
-                            Greeting(name = name + ", indice: " + index.toString())
-                        }
-                        */
-                        Greeting(name = "Puto Color", color = Colors.Red)
-                        
-                        
-                        
+                        else -> result.text = names[y][x]
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String,color:MainActivity.Colors, modifier: Modifier = Modifier) {
-
-    Text(
-        text = "Hello ${name.uppercase()}",
-        modifier = modifier,
-        color = color.WithAlpha(1.0f)
-    )
-
-}
-/*
-fun Greeting(name: String?, name2: String?,modifier: Modifier = Modifier) {
-
-    /*
-    val name = name?.let{
-        val name2 = name2?.let{
-            Text(
-                text = "Hello ${name.uppercase()}!",
-                modifier = modifier
-            )
-        }
-    }?: run {
-        Text(
-            text = "Hello es null!",
-            modifier = modifier
-        )
-    }
-    */
-
-    val name = name ?: run {
-        Log.e("Null","name is null")
-        return
-    }
-    val name2 = name2 ?: run {
-        Log.e("Null","name2 is null")
-        return
-    }
-    Text(
-        text = "Hello ${name.uppercase()}",
-        modifier = modifier
-    )
-
-}
-*/
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HijoputaTheme {
-
     }
 }
