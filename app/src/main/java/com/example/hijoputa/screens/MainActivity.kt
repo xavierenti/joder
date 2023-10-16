3e78e9fd2ac79aa4a2176e0ca80e3a8013703d1e
xavierenti <xavier.massaneda@enti.cat>
2023-10-16 18:47:09 +0200

    Popaps

--- app/src/main/java/com/example/hijoputa/screens/MainActivity.kt
+++ app/src/main/java/com/example/hijoputa/screens/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.hijoputa.screens
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.compose.material3.AlertDialog
 import com.example.hijoputa.R
 
 class MainActivity : ComponentActivity() {
@@ -14,11 +19,52 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.start_screen)
 
         btChangeScreen.setOnClickListener{
-            val newIntent = Intent(this, SecondScreen::class.java)
-            newIntent.putExtra("textString", "cadena de texto transferida")
+            /*
+            val toast = Toast(this)
+            toast.duration = Toast.LENGTH_LONG
+            toast.setText("Soy un toast muy molon")
 
-            this .startActivity (newIntent)
-            overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left)
+            toast.show()
+            */
+
+            var builder = AlertDialog.Builder(this)
+            builder.setTitle("Puto titulo del dialogo")
+            //builder.setMessage("Mensage todavia mas puto del dialogo")
+            builder.setPositiveButton("Puto Positivo"){dialoge, witch ->
+                dialoge.dismiss()
+            }
+
+            builder.setOnCancelListener{
+                val toast = Toast(this)
+                toast.setText("Puto Cancelado")
+                toast.show()
+            }
+
+            builder.setNegativeButton("Puto Final"){dialoge, which ->
+                dialoge.dismiss()
+            }
+
+            builder.setNeutralButton("Puto Neutro"){dialoge, which ->
+                dialoge.dismiss()
+            }
+
+            val options = arrayOf("Respuesta 1", "Respuesta 2", "Respuesta 3", "Respuesta 4")
+            val selectedAnswers = booleanArrayOf (false, false, false, false)
+
+            /*builder.setMultiChoiceItems(options, selectedAnswers){dialog, which, isChecked ->
+                selectedAnswers[which] = isChecked
+            }
+*/
+
+            /*val input = EditText(this)
+            input.hint = "Escribe puto"
+            builder.show(input)
+            */
+
+                
+
+            val dialog = builder.create()
+            dialog.show()
         }
     }
 }
